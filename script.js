550fafe5325d9c4a48d95670857cdc640eea140a
botondracz <aventcamp@gmail.com>
2020-04-29 14:55:56 +0200

    insert first part

---
+++ script.js
@@ -0,0 +1,33 @@
+let name = 'Adam';
+
+console.log(name + "'s apples");
+console.log('Hello World!');
+
+//this is a JavaScript comment
+
+let person = {}
+undefined
+let person = {
+name:'Adam',
+age: 33,
+}
+undefined
+let person = {
+name:'Adam',
+age: 33,
+}
+undefined
+person
+{name: "Adam", age: 33}
+person.name
+"Adam"
+person.age
+33
+person.age + 10
+43
+BackgroundFetchManager
+ƒ BackgroundFetchManager() { [native code] }
+person.color = ['lime', 'green']
+(2) ["lime", "green"]
+person
+{name: "Adam", age: 33, color: Array(2)}age: 33color: (2) ["lime", "green"]name: "Adam"__proto__: Object
